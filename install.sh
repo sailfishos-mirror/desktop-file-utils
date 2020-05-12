46d5759e3c81c610cb9c49b212e744a10ff9553a
Félix Piédallu <felix@piedallu.me>
2020-05-12 11:27:50 +0200

    Use python for scripts instead of bash

--- install.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-ln -sf "desktop-file-install" \
-    "${MESON_INSTALL_DESTDIR_PREFIX}/bin/desktop-file-edit"
--- make_dist.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-git log --no-color -M -C --name-status \
-    > "${MESON_DIST_ROOT}/ChangeLog"
